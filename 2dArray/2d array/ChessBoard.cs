3f4c5e4f3943c77bbe8e288d528070fb334728c9
Leifsgard <Leifsgard@outlook.com>
2014-11-14 12:07:34 +0100

    Jag fixade lite utav en meny

--- 2dArray/2d array/ChessBoard.cs
+++ 2dArray/2d array/ChessBoard.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace _2d_array
@@ -13,25 +16,47 @@ namespace _2d_array
         public string[,] board = new string[x, y];
 
     public void PrintBoard()
-        {
+    {
+
+
+
+
+
+
+        
+
+            Console.WriteLine("   AlphaChess v.1.33.7 | Copyright 2014 c The Geeks at EC");
+            Console.WriteLine("   ----------------------------------------");
             Console.WriteLine("                Black Side   ");
-            Console.WriteLine("[{0}] [{1}] [{2}] [{3}] [{4}] [{5}] [{6}] [{7}]", board[0, 0], board[0, 1], board[0, 2],
+            Console.WriteLine("");
+            Console.Write("8 | ");
+       Console.WriteLine("[{0}] [{1}] [{2}] [{3}] [{4}] [{5}] [{6}] [{7}]", board[0, 0], board[0, 1], board[0, 2],
                 board[0, 3], board[0, 4], board[0, 5], board[0, 6], board[0, 7]);
-            Console.WriteLine("[{0}] [{1}] [{2}] [{3}] [{4}] [{5}] [{6}] [{7}]", board[1, 0], board[1, 1], board[1, 2],
+            Console.Write("7 | ");  
+       Console.WriteLine("[{0}] [{1}] [{2}] [{3}] [{4}] [{5}] [{6}] [{7}]", board[1, 0], board[1, 1], board[1, 2],
                 board[1, 3], board[1, 4], board[1, 5], board[1, 6], board[1, 7]);
-            Console.WriteLine("[ {0} ] [ {1} ] [ {2} ] [ {3} ] [ {4} ] [ {5} ] [ {6} ] [ {7} ]", board[2, 0], board[2, 1], board[2, 2],
+            Console.Write("6 | ");     
+        Console.WriteLine("[ {0} ] [ {1} ] [ {2} ] [ {3} ] [ {4} ] [ {5} ] [ {6} ] [ {7} ]", board[2, 0], board[2, 1], board[2, 2],
                 board[2, 3], board[2, 4], board[2, 5], board[2, 6], board[2, 7]);
-            Console.WriteLine("[ {0} ] [ {1} ] [ {2} ] [ {3} ] [ {4} ] [ {5} ] [ {6} ] [ {7} ]", board[3, 0], board[3, 1], board[3, 2],
+            Console.Write("5 | ");  
+        Console.WriteLine("[ {0} ] [ {1} ] [ {2} ] [ {3} ] [ {4} ] [ {5} ] [ {6} ] [ {7} ]", board[3, 0], board[3, 1], board[3, 2],
                 board[3, 3], board[3, 4], board[3, 5], board[3, 6], board[3, 7]);
-            Console.WriteLine("[ {0} ] [ {1} ] [ {2} ] [ {3} ] [ {4} ] [ {5} ] [ {6} ] [ {7} ]", board[4, 0], board[4, 1], board[4, 2],
+            Console.Write("4 | ");     
+        Console.WriteLine("[ {0} ] [ {1} ] [ {2} ] [ {3} ] [ {4} ] [ {5} ] [ {6} ] [ {7} ]", board[4, 0], board[4, 1], board[4, 2],
                 board[4, 3], board[4, 4], board[4, 5], board[4, 6], board[4, 7]);
-            Console.WriteLine("[ {0} ] [ {1} ] [ {2} ] [ {3} ] [ {4} ] [ {5} ] [ {6} ] [ {7} ]", board[5, 0], board[5, 1], board[5, 2],
+            Console.Write("3 | ");     
+        Console.WriteLine("[ {0} ] [ {1} ] [ {2} ] [ {3} ] [ {4} ] [ {5} ] [ {6} ] [ {7} ]", board[5, 0], board[5, 1], board[5, 2],
                 board[5, 3], board[5, 4], board[5, 5], board[5, 6], board[5, 7]);
-            Console.WriteLine("[{0}] [{1}] [{2}] [{3}] [{4}] [{5}] [{6}] [{7}]", board[6, 0], board[6, 1], board[6, 2],
+            Console.Write("2 | " );    
+        Console.WriteLine("[{0}] [{1}] [{2}] [{3}] [{4}] [{5}] [{6}] [{7}]", board[6, 0], board[6, 1], board[6, 2],
                 board[6, 3], board[6, 4], board[6, 5], board[6, 6], board[6, 7]);
-            Console.WriteLine("[{0}] [{1}] [{2}] [{3}] [{4}] [{5}] [{6}] [{7}]", board[7, 0], board[7, 1], board[7, 2],
+            Console.Write("1 | " );    
+        Console.WriteLine("[{0}] [{1}] [{2}] [{3}] [{4}] [{5}] [{6}] [{7}]", board[7, 0], board[7, 1], board[7, 2],
                 board[7, 3], board[7, 4], board[7, 5], board[7, 6], board[7, 7]);
-            Console.WriteLine("                White side    ");
+        Console.WriteLine("   ----------------------------------------");
+       
+        Console.Write("     A"); Console.Write("     B"); Console.Write("    C"); Console.Write("    D"); Console.Write("    E"); Console.Write("    F"); Console.Write("    G"); Console.WriteLine("    H");
+            
         }
     }
 }
--- 2dArray/2d array/Program.cs
+++ 2dArray/2d array/Program.cs
@@ -16,18 +16,113 @@ namespace _2d_array
     {
         private static void Main(string[] args)
         {
-            ChessBoard chessBoard = new ChessBoard();
-            SetupBlackSide blackSide = new SetupBlackSide();
-            SetupWhiteSide whiteSide = new SetupWhiteSide();
-            whiteSide.DefaultSetupW(chessBoard.board);
-            blackSide.DefaultSetup(chessBoard.board);
-            while (true)
+
+
+            // DETTA ÄR MENYN
+        Console.WriteLine("   AlphaChess v.1.33.7 | Copyright 2014 c The Geeks at EC");
+        Console.WriteLine("   ----------------------------------");
+        Console.WriteLine("   What do you want to do?");
+        Console.WriteLine("   P.Play a game");
+        Console.WriteLine("   I.Instructions");
+        Console.WriteLine("   X.Exit");
+
+
+var input = Console.ReadKey();
+
+            switch (input.Key) //Switch on Key enum
             {
-                chessBoard.PrintBoard();
-                Console.WriteLine("---------------------------------------");
-                string choice = Console.ReadLine();
-                Console.Clear();
+
+                    //PLAY
+                case ConsoleKey.P:
+                    Console.Clear();
+
+
+
+
+
+
+                    break;
+                    //EXIT
+                case ConsoleKey.X:
+
+                    // HÄR SKA FINNAS EN SHUTDOWN FÖR PROGRAMMET
+                   
+                    break;
+               
+              // INSTUKTIONER
+                case ConsoleKey.I:
+                    Console.Clear();
+                    Console.WriteLine("   CHESS RULES & HOW-TO");
+                    Console.WriteLine(" ----------------------------------");
+                    Console.WriteLine(" Your objective is to kill the black king,");
+                    Console.WriteLine(" it might sound very racist but it is not.");
+                    Console.WriteLine(" Whenever something is 'killed', the square is replaced by your own chesspiece");
+                    Console.WriteLine("");
+
+                    Console.WriteLine(" There will always be 2 letters for a piece,");
+                    Console.WriteLine(" first is for color and second is for character.");
+                    Console.WriteLine("");
+                    Console.WriteLine(" Every unit on the chessboard can move in a certain pattern:");
+                    Console.WriteLine(" PAWNS aka P");
+                    Console.WriteLine(" Move: 2 or 1 squares foward at first move, afterwards only 1 foward.");
+                    Console.WriteLine(" Attack: can only attack an enemy if it stands snett framför.");
+                    Console.WriteLine("");
+                    Console.WriteLine(" Tower aka T");
+                    Console.WriteLine(" Move: Can Move all the way in both horizonal and vertical. Can be blocked");
+                    Console.WriteLine(" Attack: Move all the way untill an is reached enemy.");
+                    Console.WriteLine("");
+                    Console.WriteLine(" HORSE aka H");
+                    Console.WriteLine("Move: 2 Steps to a direction and then one square to the Right/Left");
+                    Console.WriteLine("Attack: Gotta land on the enemy to have it killed");
+                    Console.WriteLine("");
+                    Console.WriteLine(" CRUSADER aka C");
+                    Console.WriteLine("");
+                    Console.WriteLine(" QUEEN aka Q");
+                    Console.WriteLine("");
+                    Console.WriteLine(" KING aka K");
+                    Console.WriteLine("");
+
+                    // SVAR PÅ OM ALLT ÄR FÖRSTÅTT
+                    Console.WriteLine(" Is it all understood?");
+                    var answer = Console.ReadKey();
+                    
+                switch (answer.Key)
+                    {
+                        case ConsoleKey.Y:
+                             Console.WriteLine("");
+                             Console.WriteLine("Great, let's game!");
+                             
+                            break;
+                        case ConsoleKey.N:
+                            Console.Clear();
+                            Console.WriteLine("Oh.. I will explain it one more time! But more simple this time.. ");
+                            break;
+                    }
+                   
+
+                    break;
             }
+
+
+            // SPELET STARTAR 
+      //  ChessBoard chessBoard = new ChessBoard();
+      //  SetupBlackSide blackside = new SetupBlackSide();
+      //  SetupWhiteSide whiteSide = new SetupWhiteSide();
+      //  whiteSide.DefaultSetupW(chessBoard.board);
+      //  blackside.DefaultSetup(chessBoard.board);
+      //
+      //  while (true)
+      //  {
+      //      chessBoard.PrintBoard();
+      //      Console.WriteLine("   ----------------------------------------");
+      //      Console.WriteLine("    Which chesspiece do you want to move?");
+      //      var whatmove = Console.ReadLine();
+      //      Console.WriteLine("    Where do you want to move it? ");
+      //      var moveto = Console.ReadKey();
+      //
+      //      string choice = Console.ReadLine();
+      //      Console.Clear();
+      //  }
         }
     }
 }
