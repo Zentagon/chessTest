5551c7de9525acf612a8cd65a6c56687c45b3f8c
Ageofempierswar <stoffe_data@hotmail.com>
2014-11-17 14:14:34 +0100

    tog bort

--- 2dArray/2d array/Bishop.cs
+++ 2dArray/2d array/Bishop.cs
@@ -8,30 +8,40 @@ namespace _2d_array
 {
     public class Bishop: IChessPiece
     {
-        public string Name()
+          public string Name="B";
+        public string Color;
+        public Position position;
+ 
+        public Bishop(string Color, Position pos)
         {
-            string name = "B";
-            return Color() + name;
-        }
-        public string Color()
-        {
-            string color = "B";
-            return color;
+            this.Color = Color;
+            position = pos;
         }
 
-        public string Name2()
-        {
-            string name2 = "B";
-            return Color2() + name2;
-        }
-        public string Color2()
-        {
-            string color2 = "W";
-            return color2;
-        }
-        public void Move()
-        {
-            //DoStuff
-        }
+        //public string Name()
+        //{
+        //    string name = "B";
+        //    return Color() + name;
+        //}
+        //public string Color()
+        //{
+        //    string color = "B";
+        //    return color;
+        //}
+
+        //public string Name2()
+        //{
+        //    string name2 = "B";
+        //    return Color2() + name2;
+        //}
+        //public string Color2()
+        //{
+        //    string color2 = "W";
+        //    return color2;
+        //}
+        //public void Move()
+        //{
+        //    //DoStuff
+        //}
     }
 }
--- 2dArray/2d array/Queen.cs
+++ 2dArray/2d array/Queen.cs
@@ -8,33 +8,43 @@ namespace _2d_array
 {
     public class Queen : IChessPiece
     {
-        public string Name()
+          public string Name="Q";
+        public string Color;
+        public Position position;
+ 
+        public Queen(string Color, Position pos)
         {
-            string name = "Q";
-            return Color() + name;
+            this.Color = Color;
+            position = pos;
         }
 
-        public string Color()
-        {
-            string color = "B";
-            return color;
+        //public string Name()
+        //{
+        //    string name = "Q";
+        //    return Color() + name;
+        //}
 
-        }
+        //public string Color()
+        //{
+        //    string color = "B";
+        //    return color;
 
-        public string Name2()
-        {
-            string name2 = "Q";
-            return Color2() + name2;
-        }
-        public string Color2()
-        {
-            string color2 = "W";
-            return color2;
-        }
+        //}
 
-        public void Move()
-        {
-            //DoStuff
-        }
+        //public string Name2()
+        //{
+        //    string name2 = "Q";
+        //    return Color2() + name2;
+        //}
+        //public string Color2()
+        //{
+        //    string color2 = "W";
+        //    return color2;
+        //}
+
+        //public void Move()
+        //{
+        //    //DoStuff
+        //}
     }
 }
