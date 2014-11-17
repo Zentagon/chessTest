9106fb67365eec303c9358791fe8690562d4e006
Ageofempierswar <stoffe_data@hotmail.com>
2014-11-17 14:10:15 +0100

    La till fler klasser och ska fixa mer med koderna
    LOLFTW

---
+++ 2dArray/2d array/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2d_array
+{
+    class Player
+    {
+        public List<Peasant> Piece = new List<Peasant>();
+        public string Color;
+
+        public Player(string Color)
+        {
+            this.Color = Color;
+            Peasant P1 = new Peasant("W", new Position(1, 1));
+            Peasant P2 = new Peasant("W", new Position(2, 1));
+            Peasant P3 = new Peasant("W", new Position(3, 1));
+            Peasant P4 = new Peasant("W", new Position(4, 1));
+            Peasant P5 = new Peasant("W", new Position(5, 1));
+            Peasant P6 = new Peasant("W", new Position(6, 1));
+            Peasant P7 = new Peasant("W", new Position(7, 1));
+            Peasant P8 = new Peasant("W", new Position(8, 1));
+            Piece.Add(P1);
+            Piece.Add(P2);
+            Piece.Add(P3);
+            Piece.Add(P4);
+            Piece.Add(P5);
+            Piece.Add(P6);
+            Piece.Add(P7);
+            Piece.Add(P8);
+        }
+
+        public List<Peasant> GetPieces()
+        {
+            return Piece;
+        }
+    }
+}
--- 2dArray/2d array/SetupWhiteSide.cs
+++ 2dArray/2d array/SetupWhiteSide.cs
@@ -8,60 +8,60 @@ namespace _2d_array
 {
     class SetupWhiteSide
     {
-        public string DefaultSetupW(string[,] input)
-        {
-            int x = 8;
-            int y = 8;
-            string[,] board = new string[x, y];
-            Peasant WhiteBonde1 = new Peasant();
-            Peasant WhiteBonde2 = new Peasant();
-            Peasant WhiteBonde3 = new Peasant();
-            Peasant WhiteBonde4 = new Peasant();
-            Peasant WhiteBonde5 = new Peasant();
-            Peasant WhiteBonde6 = new Peasant();
-            Peasant WhiteBonde7 = new Peasant();
-            Peasant WhiteBonde8 = new Peasant();
-            King king = new King();
-            Queen queen = new Queen();
-            Bishop bishop = new Bishop();
-            Bishop bishop2 = new Bishop();
-            Tower tower = new Tower();
-            Tower tower2 = new Tower();
-            Crusader crusader = new Crusader();
-            Crusader crusader2 = new Crusader();
-            board[7, 5] = bishop2.Name2();
-            board[7, 2] = bishop.Name2();
-            board[7, 7] = tower2.Name2();
-            board[7, 0] = tower.Name2();
-            board[7, 3] = queen.Name2();
-            board[7, 4] = king.Name2();
-            board[7, 6] = crusader2.Name2();
-            board[7, 1] = crusader.Name2();
-            board[6, 0] = WhiteBonde1.Name2();
-            board[6, 1] = WhiteBonde2.Name2();
-            board[6, 2] = WhiteBonde3.Name2();
-            board[6, 3] = WhiteBonde4.Name2();
-            board[6, 4] = WhiteBonde5.Name2();
-            board[6, 5] = WhiteBonde6.Name2();
-            board[6, 6] = WhiteBonde7.Name2();
-            board[6, 7] = WhiteBonde8.Name2();
-            input[6, 0] = board[6, 0];
-            input[6, 1] = board[6, 1];
-            input[6, 2] = board[6, 2];
-            input[6, 3] = board[6, 3];
-            input[6, 4] = board[6, 4];
-            input[6, 5] = board[6, 5];
-            input[6, 6] = board[6, 6];
-            input[6, 7] = board[6, 7];
-            input[7, 5] = board[7, 5];
-            input[7, 2] = board[7, 2];
-            input[7, 7] = board[7, 7];
-            input[7, 0] = board[7, 0];
-            input[7, 3] = board[7, 3];
-            input[7, 4] = board[7, 4];
-            input[7, 6] = board[7, 6];
-            input[7, 1] = board[7, 1];
-            return input[0, 0];
+        //public string DefaultSetupW(string[,] input)
+        //{
+        //    int x = 8;
+        //    int y = 8;
+        //    string[,] board = new string[x, y];
+        //    Peasant WhiteBonde1 = new Peasant();
+        //    Peasant WhiteBonde2 = new Peasant();
+        //    Peasant WhiteBonde3 = new Peasant();
+        //    Peasant WhiteBonde4 = new Peasant();
+        //    Peasant WhiteBonde5 = new Peasant();
+        //    Peasant WhiteBonde6 = new Peasant();
+        //    Peasant WhiteBonde7 = new Peasant();
+        //    Peasant WhiteBonde8 = new Peasant();
+        //    King king = new King();
+        //    Queen queen = new Queen();
+        //    Bishop bishop = new Bishop();
+        //    Bishop bishop2 = new Bishop();
+        //    Tower tower = new Tower();
+        //    Tower tower2 = new Tower();
+        //    Crusader crusader = new Crusader();
+        //    Crusader crusader2 = new Crusader();
+        //    board[7, 5] = bishop2.Name2();
+        //    board[7, 2] = bishop.Name2();
+        //    board[7, 7] = tower2.Name2();
+        //    board[7, 0] = tower.Name2();
+        //    board[7, 3] = queen.Name2();
+        //    board[7, 4] = king.Name2();
+        //    board[7, 6] = crusader2.Name2();
+        //    board[7, 1] = crusader.Name2();
+        //    board[6, 0] = WhiteBonde1.Name2();
+        //    board[6, 1] = WhiteBonde2.Name2();
+        //    board[6, 2] = WhiteBonde3.Name2();
+        //    board[6, 3] = WhiteBonde4.Name2();
+        //    board[6, 4] = WhiteBonde5.Name2();
+        //    board[6, 5] = WhiteBonde6.Name2();
+        //    board[6, 6] = WhiteBonde7.Name2();
+        //    board[6, 7] = WhiteBonde8.Name2();
+        //    input[6, 0] = board[6, 0];
+        //    input[6, 1] = board[6, 1];
+        //    input[6, 2] = board[6, 2];
+        //    input[6, 3] = board[6, 3];
+        //    input[6, 4] = board[6, 4];
+        //    input[6, 5] = board[6, 5];
+        //    input[6, 6] = board[6, 6];
+        //    input[6, 7] = board[6, 7];
+        //    input[7, 5] = board[7, 5];
+        //    input[7, 2] = board[7, 2];
+        //    input[7, 7] = board[7, 7];
+        //    input[7, 0] = board[7, 0];
+        //    input[7, 3] = board[7, 3];
+        //    input[7, 4] = board[7, 4];
+        //    input[7, 6] = board[7, 6];
+        //    input[7, 1] = board[7, 1];
+        //    return input[0, 0];
         }
     }
 }
