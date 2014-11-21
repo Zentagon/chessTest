29f2c280a9b5bbbefe65d2d80be3b83a0d7d0d7c
Ageofempierswar <stoffe_data@hotmail.com>
2014-11-21 11:24:14 +0100

    W
    S

--- 2dArray/2d array/används ej - Player.cs
+++ 2dArray/2d array/används ej - Player.cs
@@ -14,22 +14,38 @@ namespace _2d_array
         public Player(string Color)
         {
             this.Color = Color;
-            Peasant P1 = new Peasant("W", new Position(1, 1));
-            Peasant P2 = new Peasant("W", new Position(2, 1));
-            Peasant P3 = new Peasant("W", new Position(3, 1));
-            Peasant P4 = new Peasant("W", new Position(4, 1));
-            Peasant P5 = new Peasant("W", new Position(5, 1));
-            Peasant P6 = new Peasant("W", new Position(6, 1));
-            Peasant P7 = new Peasant("W", new Position(7, 1));
-            Peasant P8 = new Peasant("W", new Position(8, 1));
-            Piece.Add(P1);
-            Piece.Add(P2);
-            Piece.Add(P3);
-            Piece.Add(P4);
-            Piece.Add(P5);
-            Piece.Add(P6);
-            Piece.Add(P7);
-            Piece.Add(P8);
+            Peasant WP1 = new Peasant("W", new Position(1, 1));
+            Peasant WP2 = new Peasant("W", new Position(2, 1));
+            Peasant WP3 = new Peasant("W", new Position(3, 1));
+            Peasant WP4 = new Peasant("W", new Position(4, 1));
+            Peasant WP5 = new Peasant("W", new Position(5, 1));
+            Peasant WP6 = new Peasant("W", new Position(6, 1));
+            Peasant WP7 = new Peasant("W", new Position(7, 1));
+            Peasant WP8 = new Peasant("W", new Position(8, 1));
+            Piece.Add(WP1);
+            Piece.Add(WP2);
+            Piece.Add(WP3);
+            Piece.Add(WP4);
+            Piece.Add(WP5);
+            Piece.Add(WP6);
+            Piece.Add(WP7);
+            Piece.Add(WP8);
+            Peasant BP1 = new Peasant("B", new Position(1, 6));
+            Peasant BP2 = new Peasant("B", new Position(2, 6));
+            Peasant BP3 = new Peasant("B", new Position(3, 6));
+            Peasant BP4 = new Peasant("B", new Position(4, 6));
+            Peasant BP5 = new Peasant("B", new Position(5, 6));
+            Peasant BP6 = new Peasant("B", new Position(6, 6));
+            Peasant BP7 = new Peasant("B", new Position(7, 6));
+            Peasant BP8 = new Peasant("B", new Position(8, 6));
+            Piece.Add(BP1);
+            Piece.Add(BP2);
+            Piece.Add(BP3);
+            Piece.Add(BP4);
+            Piece.Add(BP5);
+            Piece.Add(BP6);
+            Piece.Add(BP7);
+            Piece.Add(BP8);
         }
 
         public List<Peasant> GetPieces()
